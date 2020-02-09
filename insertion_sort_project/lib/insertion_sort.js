058846caf0d736ec76f94f10091befab4aee231b
Elson Li <elson.li@berkeley.edu>
2020-02-08 21:57:33 -0800

    Pass Mocha tests

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,7 +1,14 @@
-function insertionSort(arr) {
-
+const insertionSort = array => {
+  for (let idx = 1; idx < array.length; idx++) {
+    const currEle = array[idx];
+    for (var jdx = idx - 1; jdx >= 0 && currEle < array[jdx]; jdx--) {
+      array[jdx + 1] = array[jdx];
+    }
+    array[jdx + 1] = currEle;
+  }
+  return array;
 }
 
 module.exports = {
-    insertionSort
+  insertionSort
 };
