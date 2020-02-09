98af9e8e95f000277aba34aabd82eb31f990d295
Elson Li <elson.li@berkeley.edu>
2020-02-08 22:55:00 -0800

    Pass Mocha tests

---
+++ merge_sort_project/lib/merge_sort.js
@@ -0,0 +1,24 @@
+const merge = (array1, array2) => {
+  let mergedArray = [];
+  while (array1.length && array2.length) {
+    if (array1[0] < array2[0]) {
+      mergedArray.push(array1.shift());
+    } else {
+      mergedArray.push(array2.shift());
+    }
+  }
+  return mergedArray.concat(array1, array2);
+}
+
+const mergeSort = array => {
+  if (!array.length || array.length == 1) return array;
+  let middleIdx = Math.floor(array.length / 2);
+  let mergedLeft = mergeSort(array.slice(0, middleIdx));
+  let mergedRight = mergeSort(array.slice(middleIdx));
+  return merge(mergedLeft, mergedRight);
+}
+
+module.exports = {
+    merge,
+    mergeSort
+};
