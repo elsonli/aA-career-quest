ea1958b739fbc32767c7a42437725a458d914c87
Elson Li <elson.li@berkeley.edu>
2020-02-08 21:09:02 -0800

    Pass Mocha tests

--- bubble_sort_project/lib/bubble_sort.js
+++ bubble_sort_project/lib/bubble_sort.js
@@ -1,11 +1,21 @@
-function swap(array, idx1, idx2) {
-
+const swap = (array, idx1, idx2) => {
+  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+  return array;
 }
 
-function bubbleSort(array) {
-
+const bubbleSort = array => {
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let idx = 0; idx < array.length - 1; idx++) {
+      if (array[idx] > array[idx + 1]) {
+        swap(array, idx, idx + 1);
+        sorted = false;
+      }
+    }
+  }
+  return array;
 }
-
 
 module.exports = {
     bubbleSort,
