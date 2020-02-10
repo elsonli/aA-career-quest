7f75389db9daf06ae3e4c19cb2e22cd2d282db51
Elson Li <elson.li@berkeley.edu>
2020-02-10 15:27:28 -0800

    Close output LeetCode

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -3,7 +3,18 @@
 
 const { TreeNode } = require('./tree_node.js');
 
-
-function buildTree(preorder, inorder) {
-
+const buildTree = (preorder, inorder) => {
+  if (!preorder.length && !inorder.length) return [];
+  const rootVal = preorder.shift(); // 3
+  const rootValIdx = inorder.indexOf(rootVal); // 1
+  const leftVals = inorder.slice(0, rootValIdx); // [9]
+  const rightVals = inorder.slice(rootValIdx + 1); // [15, 20, 7]
+  const newRoot = new TreeNode(rootVal); // TreeNode(3)
+  newRoot.left = buildTree(preorder.slice(0, rootValIdx + 1), leftVals);
+  newRoot.right = buildTree(preorder.slice(rootValIdx), rightVals);
+  return newRoot;
 }
+
+preorder = [3, 9, 20, 15, 7]
+inorder = [9, 3, 15, 20, 7]
+console.log(buildTree(preorder, inorder));
--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,11 +1,16 @@
-function inOrderArray(root) {
-
+const inOrderArray = root => {
+  if (!root) return [];
+  const recurseLeft = inOrderArray(root.left);
+  const recurseRight = inOrderArray(root.right);
+  return [ ...recurseLeft, root.val, ...recurseRight ];
 }
 
-function postOrderArray(root) {
-
+const postOrderArray = root => {
+  if (!root) return [];
+  const recurseLeft = postOrderArray(root.left);
+  const recurseRight = postOrderArray(root.right);
+  return [ ...recurseLeft, ...recurseRight, root.val ];
 }
-
 
 module.exports = {
   inOrderArray,
