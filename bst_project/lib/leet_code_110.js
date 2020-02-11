0a60750aa590c3812402b8a76a6e0483bf268be4
Elson Li <elson.li@berkeley.edu>
2020-02-10 19:43:47 -0800

    Pass LeetCode Q1

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -1,7 +1,19 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+const getHeight = root => {
+  if (!root) return 0;
+  const leftHeight = 1 + getHeight(root.left);
+  const rightHeight = 1 + getHeight(root.right);
+  return Math.max(leftHeight, rightHeight);
+}
 
-function isBalanced(root) {
-
+const isBalanced = root => {
+  if (!root) return true;
+  if (!root.left && !root.right) return true;
+  return (
+    isBalanced(root.left) &&
+    isBalanced(root.right) &&
+    Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1
+  );
 }
