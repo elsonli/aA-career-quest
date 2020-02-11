0d6d944f7920af68576d4f525f8fde3e7734cdfa
Elson Li <elson.li@berkeley.edu>
2020-02-10 20:03:30 -0800

    Pass Mocha tests

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -2,5 +2,10 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 const sortedArrayToBST = nums => {
-  
-}
+  if (!nums.length) return null;
+  const middleIdx = Math.floor(nums.length / 2);
+  const newRoot = new TreeNode(nums[middleIdx]);
+  newRoot.left = sortedArrayToBST(nums.slice(0, middleIdx));
+  newRoot.right = sortedArrayToBST(nums.slice(middleIdx + 1));
+  return newRoot;
+};
