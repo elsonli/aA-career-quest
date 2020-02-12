f4d2a38c20a4325bccefd18e7a4d4ff8e963118e
Elson Li <elson.li@berkeley.edu>
2020-02-11 20:18:26 -0800

    Pass Mocha tests

--- heaps_project/lib/is_heap.js
+++ heaps_project/lib/is_heap.js
@@ -1,9 +1,18 @@
 // you may assume that the array will always have a null element at the 0-th index
-function isMaxHeap(array, idx=1) {
+const isMaxHeap = (array) => {
+  if (array.length === 1) return true;
 
+  for (let idx = 1; idx < array.length / 2; idx++) {
+    let currVal = array[idx];
+    let leftChildIdx = idx * 2;
+    let rightChildIdx = idx * 2 + 1;
+    let leftChildVal = array[leftChildIdx] || -Infinity;
+    let rightChildVal = array[rightChildIdx] || -Infinity;
+    if (currVal < leftChildVal || currVal < rightChildVal) return false;
+  }
+  return true;
 }
 
-
 module.exports = {
-    isMaxHeap
+  isMaxHeap
 };
--- heaps_project/lib/max_heap.js
+++ heaps_project/lib/max_heap.js
@@ -1,7 +1,54 @@
 class MaxHeap {
-    
+  constructor() {
+    this.array = [null];
+  }
+
+  getParent(idx) {
+    return Math.floor(idx / 2);
+  }
+
+  getLeftChild(idx) {
+    return idx * 2;
+  }
+
+  getRightChild(idx) {
+    return idx * 2 + 1;
+  }
+
+  siftUp(idx) {
+    let parentIdx = this.getParent(idx);
+    if (idx === 1 || this.array[idx] <= this.array[parentIdx]) return;
+    [this.array[idx], this.array[parentIdx]] = [this.array[parentIdx], this.array[idx]];
+    this.siftUp(parentIdx);
+  }
+
+  insert(val) {
+    this.array.push(val);
+    this.siftUp(this.array.length - 1);
+  }
+
+  siftDown(idx) {
+    if (idx === this.array.length - 1) return;
+    let leftChildIdx = this.getLeftChild(idx);
+    let rightChildIdx = this.getRightChild(idx);
+    let leftChildVal = this.array[leftChildIdx] || -Infinity;
+    let rightChildVal = this.array[rightChildIdx] || -Infinity;
+    let greaterChildIdx = (leftChildVal > rightChildVal) ? leftChildIdx : rightChildIdx;
+    if (this.array[idx] <= this.array[greaterChildIdx]) {
+      [this.array[idx], this.array[greaterChildIdx]] = [this.array[greaterChildIdx], this.array[idx]];
+      this.siftDown(greaterChildIdx);
+    }
+  }
+
+  deleteMax() {
+    if (this.array.length === 1) return null;
+    [this.array[1], this.array[this.array.length - 1]] = [this.array[this.array.length - 1], this.array[1]];
+    let returnVal = this.array.pop();
+    this.siftDown(1);
+    return returnVal;
+  }
 }
 
 module.exports = {
-    MaxHeap
+  MaxHeap
 };
