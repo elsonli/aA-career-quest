acd4646f7c14d6931a5dbed1d61b34da1b252c46
Elson Li <elson.li@berkeley.edu>
2020-02-12 16:12:59 -0800

    Pass LeetCode

--- trie_project/lib/leet_code_208.js
+++ trie_project/lib/leet_code_208.js
@@ -1,20 +1,54 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/implement-trie-prefix-tree/
 
+const { Node } = require("../lib/trie");
+
 class Trie {
   constructor() {
-
+    this.root = new Node();
   }
 
   insert(word) {
-
+    let currNode = this.root;
+    for (let idx = 0; idx < word.length; idx++) {
+      let currLetter = word[idx];
+      if (!Object.keys(currNode.children).includes(currLetter)) {
+        currNode.children[currLetter] = new Node();
+      }
+      currNode = currNode.children[currLetter];
+    }
+    currNode.isTerminal = true;
   }
 
   search(word) {
-
+    let currNode = this.root;
+    for (let idx = 0; idx < word.length; idx++) {
+      let currLetter = word[idx];
+      if (!Object.keys(currNode.children).includes(currLetter)) {
+        return false;
+      }
+      currNode = currNode.children[currLetter];
+    }
+    return currNode.isTerminal;
   }
 
   startsWith(prefix) {
-    
+    let currNode = this.root;
+    for (let idx = 0; idx < prefix.length; idx++) {
+      let currLetter = prefix[idx];
+      if (!Object.keys(currNode.children).includes(currLetter)) {
+        return false;
+      }
+      currNode = currNode.children[currLetter];
+    }
+    return this.checkTerminals(currNode);
+  }
+
+  checkTerminals(root) {
+    if (root.isTerminal) return true;
+    return Object.keys(root.children).some(letter => {
+      let traverseTo = root.children[letter];
+      return this.checkTerminals(traverseTo);
+    });
   }
 }
